a5eea08dd3d1d82e4c0449dfd762b4ea0d7fd198
Arax1 <anandraju55@hotmail.com>
2020-03-05 15:05:57 -0500

    Updates

---
+++ precache-manifest.ee80e193f476b7257f2b9cb4dba4e5b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed927533283452eb4042fbbbcd62adb5",
+    "url": "/ReactTutorial/robofriends/index.html"
+  },
+  {
+    "revision": "55c476883bdd969f6871",
+    "url": "/ReactTutorial/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ad4e605a67430f499087",
+    "url": "/ReactTutorial/robofriends/static/css/main.a3ac44ab.chunk.css"
+  },
+  {
+    "revision": "55c476883bdd969f6871",
+    "url": "/ReactTutorial/robofriends/static/js/2.5c1a6be5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactTutorial/robofriends/static/js/2.5c1a6be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4e605a67430f499087",
+    "url": "/ReactTutorial/robofriends/static/js/main.2e256fbe.chunk.js"
+  },
+  {
+    "revision": "c21fc95739b705ea926f",
+    "url": "/ReactTutorial/robofriends/static/js/runtime-main.5ccc70e3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactTutorial/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
